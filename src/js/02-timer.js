8232910dfa77f3ecac8fcf4d0b941ae37147019f
PiLoT-P <59173880+PiLoT-P@users.noreply.github.com>
2023-03-02 16:44:25 +0200

    update2zav

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-const date = new Date();
+const dateNow = Date.now();
 const startButton = document.querySelector('button[data-start]');
 let TIME ;
 
@@ -14,12 +14,13 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        if (selectedDates[0].getTime() < date.getTime()) {
+        if (selectedDates[0].getTime() < dateNow) {
            alert('Please choose a date in the future');
+        } else {
+            startButton.disabled = false;
+            console.log(selectedDates[0]); 
+            TIME = selectedDates[0].getTime();
         }
-        startButton.disabled = false;
-        console.log(selectedDates[0]); 
-        TIME = selectedDates[0].getTime();
     },
 };
 
@@ -44,9 +45,30 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
+function addLeadingZero(value){
+    if (value < 10) {
+        value = `0${value}`;
+        return value;
+    } else {
+        return value;
+    }
+}
 
 function timer(event) {
-    console.log(convertMs(TIME-date.getTime()));
+    startButton.disabled = true;
+    const timer = setInterval(() => {
+        const now = Date.now();
+        const timerTime = convertMs(TIME - now);
+        console.log(TIME - now);
+        if (TIME - now <= 0) {
+            clearInterval(timer);
+            return;
+        }
+        document.querySelector('span[data-days]').textContent = addLeadingZero(timerTime.days);
+        document.querySelector('span[data-hours]').textContent = addLeadingZero(timerTime.hours);
+        document.querySelector('span[data-minutes]').textContent = addLeadingZero(timerTime.minutes);
+        document.querySelector('span[data-seconds]').textContent = addLeadingZero(timerTime.seconds);
+    }, 1000);
 }
 
 startButton.addEventListener('click', timer);
